05542d63dba6e97f22ff14d15597b04e7f06502e
User1 <sujalshaikhsk@gmail.com>
2019-12-09 12:45:35 +0530

    Test controller added

--- src/main/java/com/bank/retailbank/controller/TestController.java
+++ src/main/java/com/bank/retailbank/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.bank.retailbank.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +13,14 @@ public class TestController {
 	@GetMapping(value ="/test" )
 	public String test(){
 		return "SUCCESS";
+	}
+	
+	@PostMapping(value ="/testpost" )
+	public List testPost(){
+		List<String> list =new ArrayList();
+		list.add("XXXX");
+		list.add("XYZ");
+		return list;
 	}
 	
 }
