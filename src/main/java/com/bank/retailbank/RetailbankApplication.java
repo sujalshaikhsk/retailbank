50e69be513e7b1f61e363503f2bebce6606c23a9
User1 <sujalshaikhsk@gmail.com>
2019-12-09 11:48:25 +0530

    First commit

---
+++ src/main/java/com/bank/retailbank/RetailbankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.retailbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/retailbank/RetailbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.retailbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
