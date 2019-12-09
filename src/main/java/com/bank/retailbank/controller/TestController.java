e5d77a2e5a831324b63dd5e0d2d5272a8ff33894
Shilendra <shuilu1993sr@gmail.com>
2019-12-09 12:47:34 +0530

    method added

--- src/main/java/com/bank/retailbank/controller/TestController.java
+++ src/main/java/com/bank/retailbank/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.bank.retailbank.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class TestController {
 	@GetMapping(value ="/test" )
 	public String test(){
 		return "SUCCESS";
+	}
+	
+	@PostMapping(value ="/test1" )
+	public String test1(){
+		return "test success";
 	}
 	
 }
